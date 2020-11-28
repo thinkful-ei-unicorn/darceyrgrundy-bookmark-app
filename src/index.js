b7ab0f52ff1c6ac1ddfcb383645e8e0f40e9fc57
Beyonce Knowles <you@example.com>
2020-11-28 13:54:10 -0500

    webpack changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import $ from 'jquery';
 
 function main() {
@@ -8,3 +9,34 @@ function main() {
 }
 
 $(main);
+
+//my store: used for generating my pre-made bookmarks and updating to it to get my added ones
+/*const store = {
+  bookmarks: [
+    {
+      id: 'x56w',
+      title: 'Title 1',
+      rating: 3,
+      url: 'http://www.title1.com',
+      description: 'lorem ipsum dolor sit',
+      expanded: false
+    },
+    {
+      id: '6ffw',
+      title: 'Title 2',
+      rating: 5,
+      url: 'http://www.title2.com',
+      description: 'dolorum tempore deserunt',
+      expanded: false
+    } 
+  ],
+  adding: false,
+  error: null,
+  filter: 0
+};
+
+*/
+
+//-----pseudocode/things i want my code to do------
+// i want a function that takes my store and puts my bookmarks where they belong
+
