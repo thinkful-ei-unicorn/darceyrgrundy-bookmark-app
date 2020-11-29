ba7eb0c2d444a47811bdb4271491dcb8b1e96ee4
Beyonce Knowles <you@example.com>
2020-11-28 20:13:44 -0500

    edit second

--- docs/second.js
+++ docs/second.js
@@ -1,1 +1,42 @@
 console.log('hello world');
+
+//my store: used for generating my pre-made bookmarks and updating to it to get my added ones
+/*const store = {
+  bookmarks: [
+    {
+      id: 'x56w',
+      title: 'Title 1',
+      rating: 3,
+      url: 'http://www.title1.com',
+      description: 'lorem ipsum dolor sit',
+      expanded: false
+    },
+    {
+      id: '6ffw',
+      title: 'Title 2',
+      rating: 5,
+      url: 'http://www.title2.com',
+      description: 'dolorum tempore deserunt',
+      expanded: false
+    } 
+  ],
+  adding: false,
+  error: null,
+  filter: 0
+};
+*/
+
+//-----pseudocode/things i want my code to do------
+// i want a function that takes my store and puts my bookmarks where they belong
+
+function insertPremadeBookmarks() {
+  return 'hello';
+}
+  
+function main() {
+  insertPremadeBookmarks();
+}
+  
+$(main);
+  
+console.log('hiii', main());
