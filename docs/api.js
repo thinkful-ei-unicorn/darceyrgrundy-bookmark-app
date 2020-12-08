8f37632b23e7bfdb6197a6859d7f275a47e03b16
Darcey Grundy <69948325+grundydarcey@users.noreply.github.com>
2020-12-07 20:01:32 -0500

    Create api.js

---
+++ docs/api.js
@@ -0,0 +1,67 @@
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com';
+
+function apiFetch(...args) {
+  let error;
+  return fetch(...args)
+    .then(res => {
+      if (!res.ok) {
+        error = { code: res.status };
+        if (!res.headers.get('Content-Type')) {
+          error.message = res.status;
+          return Promise.reject(error);
+        }
+      }
+      return res.json();
+    })
+    .then(data => {
+      if (error) {
+        error.message = data.message;
+        return Promise.reject(error);
+      }
+      return data;
+    });
+}
+
+
+function getItems() {
+  return apiFetch(`https://cors-anywhere.herokuapp.com/${BASE_URL}/darcey/bookmarks`, {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'GET',
+  });
+}
+
+function createItem(bookmark) {
+  return apiFetch(`https://cors-anywhere.herokuapp.com/${BASE_URL}/darcey/bookmarks`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: bookmark,
+  });
+}
+
+
+function updateItem(id, updateDataArg) {
+  const updateData = JSON.stringify(updateDataArg);
+  return apiFetch(`https://cors-anywhere.herokuapp.com/${BASE_URL}/PATCH/darcey/bookmarks/${id}`, {
+    method: 'PATCH',
+    headers: {'Content-Type': 'application/json'},
+    body: updateData,
+  });
+}
+
+
+function deleteItem(id) {
+  return apiFetch(`https://cors=anywhere.herokuapp.com/${BASE_URL}/DELETE/darcey/bookmarks/${id}`, {
+    method: 'DELETE'
+  });
+}
+
+export default {
+  getItems,
+  createItem,
+  updateItem,
+  deleteItem,
+};
