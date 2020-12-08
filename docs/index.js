4ad2543133a25ae56c6810884753da653e3436b1
Darcey Grundy <69948325+grundydarcey@users.noreply.github.com>
2020-12-08 17:07:57 -0500

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -1,8 +1,10 @@
 import $ from 'jquery';
-//import './index.css';
+import './index.css';
 import bookmarks from './bookmarks';
 import api from './api';
 import store from './store';
+import './index.css';
+import store1 from './store1';
 
 const main = function () {
   api.getItems()
