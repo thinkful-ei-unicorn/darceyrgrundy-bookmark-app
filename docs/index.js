61a2a4b4fed4720621f7ec1eb92ba8aa3cf44fa7
Darcey Grundy <69948325+grundydarcey@users.noreply.github.com>
2020-12-07 20:03:33 -0500

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -1,55 +1,19 @@
-/* eslint-disable no-console */
-console.log('hello world');
 import $ from 'jquery';
+//import './index.css';
+import bookmarks from './bookmarks';
+import api from './api';
+import store from './store';
 
-function main() {
-  console.log('DOM is loaded');
-
-  const startMsg = $('<p>Webpack is working!</p>');
-  $('#root').append(startMsg);
-}
-
-function talk() {
-  console.log('words');
-}
-
-
-  
-
-  
-$(main);
-  
-console.log('hiii', main());
-
-$(talk);
-$(main);
-
-//my store: used for generating my pre-made bookmarks and updating to it to get my added ones
-/*const store = {
-  bookmarks: [
-    {
-      id: 'x56w',
-      title: 'Title 1',
-      rating: 3,
-      url: 'http://www.title1.com',
-      description: 'lorem ipsum dolor sit',
-      expanded: false
-    },
-    {
-      id: '6ffw',
-      title: 'Title 2',
-      rating: 5,
-      url: 'http://www.title2.com',
-      description: 'dolorum tempore deserunt',
-      expanded: false
-    } 
-  ],
-  adding: false,
-  error: null,
-  filter: 0
+const main = function () {
+  api.getItems()
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      items.forEach((item) => item.expanded = false);
+      bookmarks.render();
+    })
+    .then;
+  bookmarks.allEventListeners();
+  bookmarks.render();
 };
 
-*/
-
-//-----pseudocode/things i want my code to do------
-// i want a function that takes my store and puts my bookmarks where they belong
+$(main);
