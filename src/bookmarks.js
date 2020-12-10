3046ab537977b2e9eba2f9be95e84ef52d2a3770
Beyonce Knowles <you@example.com>
2020-12-10 17:20:04 -0500

    a11y bp

--- src/bookmarks.js
+++ src/bookmarks.js
@@ -186,7 +186,7 @@ function render() {
     <div class="bookmarkview">
       <form id="startview">
         <button class="startnew" button type="button">+ Add New Bookmark</button>
-        <select id="ratings" name="ratings">
+        <select id="ratings" name="ratings" label="select-star-rating">
           <option> <span class="button-label" id="starselect">View Bookmarks By Stars</span></option>
           <option value="1">1 star</option>
           <option value="2">2 stars</option>
